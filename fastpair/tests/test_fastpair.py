a30d9f5d27f90d00c17255d7a778bb6890db12ee
James Gaboardi <jgaboardi@gmail.com>
2024-07-03 21:03:45 -0400

    Update fastpair/tests/test_fastpair.py
    Co-authored-by: cjqf <carson.farmer@gmail.com>

--- fastpair/tests/test_fastpair.py
+++ fastpair/tests/test_fastpair.py
@@ -3,7 +3,7 @@
 Testing module for FastPair.
 """
 
-# Copyright (c) 2016, Carson J. Q. Farmer <carsonfarmer@gmail.com>
+# Copyright (c) 2016, Carson J. Q. Farmer <carson.farmer@gmail.com>
 # Copyright (c) 2002-2015, David Eppstein
 # Licensed under the MIT Licence (http://opensource.org/licenses/MIT).
 
