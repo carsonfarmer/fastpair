9dd5b976b1996b981638c4c2cf63edbbf06755f8
James Gaboardi <jgaboardi@gmail.com>
2024-07-12 21:23:31 -0400

    break out types.py

--- fastpair/base.py
+++ fastpair/base.py
@@ -2,64 +2,26 @@
 # Copyright (c) 2002-2015, David Eppstein
 # Licensed under the MIT Licence (http://opensource.org/licenses/MIT).
 
-import sys
+
 from collections import defaultdict
-from collections.abc import Callable, Generator, Iterable
 from itertools import combinations, cycle
-from typing import Annotated, TypeAlias, TypedDict, TypeVar
 
-################################################################
-# See GH#73 -- remove once Python 3.11 is minimum
-if sys.version_info.minor < 11:
-    from typing_extensions import Self
-else:
-    from typing import Self
-################################################################
-
-
-import numpy
 import scipy.spatial.distance as dist
 
+from .types import (
+    AllNeighs,
+    Callable,
+    ClosestPair,
+    Dist,
+    DistNeighDict,  # noqa: F401
+    DistNeighTuple,
+    Iterable,
+    PointCoords,
+    PointsInput,
+    Self,
+)
+
 __all__ = ["FastPair", "dist"]
-
-
-# point typing ---------------------------------------------------------------------
-_Numeric = TypeVar("_Numeric", float, int, complex, numpy.number)
-NumericCoord: TypeAlias = Annotated[_Numeric, "Any numeric value."]
-
-_PointCoords = TypeVar("_PointCoords", bound=tuple[NumericCoord])
-PointCoords = Annotated[_PointCoords, "Must be a tuple of at least 2 elements."]
-
-_PointsInput = TypeVar("_PointsInput", bound=Iterable[PointCoords])
-PointsInput = Annotated[_PointsInput, "Input for ``.build()``."]
-
-
-# distance typing ------------------------------------------------------------------
-Dist: TypeAlias = Annotated[_Numeric, "Any numeric value."]
-
-_DistNeighTuple = TypeVar("_DistNeighTuple", bound=tuple[Dist, PointCoords])
-DistNeighTuple = Annotated[_DistNeighTuple, "Returned from ``.__getitem__()``."]
-
-_AllNeighs = Generator[DistNeighTuple, None, None]
-AllNeighs = Annotated[_AllNeighs, "Returned from ``.sdist()``."]
-
-
-# pair typing ----------------------------------------------------------------------
-class DistNeighDict(TypedDict):
-    """The values of the ``FastPair.neighbors`` dictionary.
-    Also potentially returned from ``._update_point()``.
-    See GH#69.
-    """
-
-    dist: Dist
-    neigh: PointCoords
-
-
-_NeighCoords = tuple[PointCoords, PointCoords]
-NeighCoords = Annotated[_NeighCoords, "2 instances of ``PointCoords``."]
-
-_ClosestPair = TypeVar("_ClosestPair", bound=tuple[Dist, NeighCoords])
-ClosestPair = Annotated[_ClosestPair, "Returned from ``.closest_pair()``."]
 
 
 class AttrDict(dict):
